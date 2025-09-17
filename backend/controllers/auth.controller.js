6c6726e8825ac72bc5ec40d0243908179b24a9c9
prajakta-humbe <165541399+prajakta-humbe@users.noreply.github.com>
2025-09-17 13:11:10 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,8 +28,8 @@ export const signUp=async (req,res)=>{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"strict",
-        secure:false
+        sameSite:"None",
+        secure:true
     })
     return res.status(201).json(user)
 
@@ -64,8 +64,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "strict",
-      secure: false, // set true if using https
+      sameSite: "None",
+      secure: true, // set true if using https
     });
 
     // 5. Send response
